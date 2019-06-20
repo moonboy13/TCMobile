eebcc248a07dc26577715f35353f0e989067902c
Kyle Conlon <conlonkyle@gmail.com>
2019-06-19 21:18:12 -0400

    Started the auto api generation process. Just have a set of test classes generating.

--- TCAPGenerator/APIGenerator.cs
+++ TCAPGenerator/APIGenerator.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
-using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace TCAPIGenerator
 {
 	class Program
 	{
+		#region Templates
+		static string _ClassHeaderTemplate =
+			"using System;" + Environment.NewLine + Environment.NewLine +
+			"namespace FOO" + Environment.NewLine + "{{" + Environment.NewLine +
+			"\tpublic class {0}" + Environment.NewLine + "\t{{" + Environment.NewLine +
+			"\t\tstring _rootPath = \"{1}\";" + Environment.NewLine;
+
+		static string _EndTemplate =
+			$"\t}}{Environment.NewLine}" +
+			"}";
+		#endregion
+
 		static string _inputFile = Path.Combine("APIDefinition", "2019.1.65998", "APIDefinition.xml");
 		// TODO: Verify these. Also, if this is done right then it belongs in its own project to be consumed by the app.
-		string _modelOutput = Path.Combine("..", "..", "DataModels", "Models");
-		string _apiOutput = Path.Combine("..", "..", "TeamCityAPI");
+		static string _modelOutput = Path.Combine("..", "..", "DataModels", "Models");
+		static string _apiOutput = Path.Combine("..", "..", "TeamCityAPI");
+		static TextInfo _TI = new CultureInfo("en-US", false).TextInfo;
+
+
 
 		/// <summary>
 		/// The goal here is that this helper will automadically generate the requisite TeamCity API(s) based off a provided
@@ -18,21 +35,30 @@ namespace TCAPIGenerator
 		/// <param name="args"></param>
 		static void Main(string[] args)
 		{
-			using(var reader = XmlReader.Create(new FileStream(_inputFile, FileMode.Open)))
+			XElement definition = XElement.Load(_inputFile);
+
+			var resources =
+					from elem in definition.Descendants("resource")
+					where !((string)elem.Attribute("path")).Trim().ToLower().Contains("swagger") &&
+						elem.Parent.Name.LocalName.Trim().ToLower() == "resources"
+					select elem;
+
+			foreach (var resource in resources)
 			{
-				while (reader.Read())
-				{
-					// Only create classes for resources and ignore the swagger resources.
-					if (reader.LocalName.Trim().ToLower().Equals("resource")
-						&& !reader.GetAttribute("path").ToLower().Trim().Contains("swagger"))
-					{
-						Console.WriteLine(reader.LocalName);
-					}
-				}
+				ProcessResource(resource);
 			}
 		}
 
-		static void ParseResource(XmlReader reader)
-		{ }
+		static void ProcessResource(XElement resource)
+		{
+			// Pull the root path off the resource. The last portion of this name will server as the class name.
+			string path = resource.Attributes().FirstOrDefault(att => att.Name.LocalName.Trim().ToLower() == "path").Value;
+			string className = _TI.ToTitleCase(path.Split('/').Last());
+			string filePath = @"/test/" + className + ".cs";
+
+			File.WriteAllText(filePath, string.Format(_ClassHeaderTemplate, className, path));
+			File.AppendAllText(filePath, _EndTemplate);
+		}
+
 	}
 }
