f377b8cf3087c33b7e1533c9710b1909d12da3df
Kyle Conlon <conlonkyle@gmail.com>
2019-10-10 09:17:30 -0400

    Beginning setup to store connection information and check it on startup.

--- TCMobile/TCMobile/App.xaml.cs
+++ TCMobile/TCMobile/App.xaml.cs
@@ -3,7 +3,12 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using TCMobile.Services;
 using TCMobile.Views;
+using Xamarin.Essentials;
 using TeamCityAPI;
+using TCConnector;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace TCMobile
 {
@@ -17,22 +22,48 @@ namespace TCMobile
 			DependencyService.Register<MockDataStore>();
 			DependencyService.Register<ProjectSummaryDataStore>();
 
+			TCConnectionData connectionInfo = GetConnectionInfo().Result;
+
 			MainPage = new MainPage();
+		}
+
+		private async Task<TCConnectionData> GetConnectionInfo()
+		{
+			var infoJson = await SecureStorage.GetAsync("TCConnection");
+			if (infoJson != null)
+			{
+				return JsonConvert.DeserializeObject<TCConnectionData>(infoJson);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		private async void StoreConnectionInfo(TCConnectionData connectionData)
+		{
+			//-- THIS NEEDS ENCRYPTION
+			await SecureStorage.SetAsync("TCConnection", JsonConvert.SerializeObject(connectionData));
 		}
 
 		protected override void OnStart()
 		{
 			// Handle when your app starts
+			// Check if a connection can be established, prompt the user if it is not.
 		}
 
 		protected override void OnSleep()
 		{
 			// Handle when your app sleeps
+			// Probably want to close any connections here
 		}
 
 		protected override void OnResume()
 		{
 			// Handle when your app resumes
+			// Reopen dem connections
 		}
+
+
 	}
 }
---
+++ TeamCityConnector/TCConnectionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamCityAPI;
+
+namespace TCConnector
+{
+	public class TCConnectionData
+	{
+		public ConnectionType ConnectionType;
+		public string Username;
+		public string Password;
+		public string AuthToken;
+	}
+}
