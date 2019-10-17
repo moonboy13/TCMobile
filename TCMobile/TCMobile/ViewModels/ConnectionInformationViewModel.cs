1fb8a646663cb3e31d32b48725b3713d63da3efb
Kyle Conlon <conlonkyle@gmail.com>
2019-10-16 21:38:32 -0400

    Having to call OnPropertyChange is going to be the death of me.

--- TCMobile/TCMobile/ViewModels/ConnectionInformationViewModel.cs
+++ TCMobile/TCMobile/ViewModels/ConnectionInformationViewModel.cs
@@ -9,8 +9,19 @@ namespace TCMobile.ViewModels
 {
 	public class ConnectionInformationViewModel : BaseViewModel<TCConnectionData>
 	{
-		public TCConnectionData ConnectionData;
-		public List<ConnectionType> ConnectionTypes;
+		private TCConnectionData _conData;
+		public TCConnectionData ConnectionData
+		{
+			get { return _conData; }
+			set { SetProperty(ref _conData, value); }
+		}
+
+		private List<ConnectionType> _conTypes;
+		public List<ConnectionType> ConnectionTypes
+		{
+			get { return _conTypes; }
+			set { SetProperty(ref _conTypes, value); }
+		}
 
 		public ConnectionInformationViewModel(TCConnectionData connectionData)
 		{
